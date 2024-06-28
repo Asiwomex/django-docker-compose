3c2b5ffec591c34b8a5a117ccb16ffcd15ddab2e
Asiwomex <118656806+Asiwomex@users.noreply.github.com>
2024-06-28 19:28:49 +0000

    updated

--- new_todo/settings.py
+++ new_todo/settings.py
@@ -50,6 +50,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+
+    "http://0.0.0.0:8000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8080"
+]
+
 ROOT_URLCONF = 'new_todo.urls'
 
 TEMPLATES = [
